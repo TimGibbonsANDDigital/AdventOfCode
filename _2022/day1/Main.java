8265df398200fe72fe9246db0197df1f26887296
Tim Gibbons <117824062+TimGibbonsANDDigital@users.noreply.github.com>
2022-12-01 11:32:44 +0000

    copy paste error

--- _2022/day1/Main.java
+++ _2022/day1/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new FileReader(new File("src/_2022.day1/input.txt")));
+        BufferedReader br = new BufferedReader(new FileReader(new File("src/_2022/day1/input.txt")));
         String line;
         Long calCount = 0L;
         Stack<Long> maxStack = new Stack<>();
